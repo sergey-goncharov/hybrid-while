00a2e42888f32deda63170d5b62908619c0c00bc
o-da <sergey.goncharov@fau.de>
2023-02-11 19:05:29 +0100

    work in progress

--- src/haskell/Hyb.hs
+++ src/haskell/Hyb.hs
@@ -1,67 +1,38 @@
 module Monads where
 import Data.Monoid
 
--- The type of hybrid computations over the state space s, with results in x
---
--- HConv d f r represents a successful trajectory f : [0,d) -> s of duration d, 
--- plus the end result r
---
--- HDiv d f represents a divergent trajectory f : [0,d) -> s of duration d
--- 
--- Because dependent pairs and subtyping are not available, trajectories are 
--- represented by functions, totally defined over all reals (positive and negative)
---
-data Hyb s x = HConv Double (Double -> s) x | HDiv Double (Double -> s)
+class (Monad m) => MonadHybrid m where
+    trajectory :: Double -> (Double -> a) -> m a
+    start      :: m a -> a
 
-evalTraj :: Hyb s x -> Double -> s
-evalTraj (HConv d f r) = f
-evalTraj (HDiv d f)    = f
+wait :: (MonadHybrid m) => Double -> a -> m a
+wait d a = trajectory d (const a)
 
-duration :: Hyb s x -> Double
-duration (HConv d f r) = d
-duration (HDiv d f)    = d
+data SymTraj  a = OdeInit a (Double -> a -> a) | Traj (Double -> a)
+data SymBound a = OdeInv (Double -> a) | Dur Double
 
-sample :: Hyb s x -> Double -> [s]
-sample p eps = h (duration p) (evalTraj p)
-  where 
-    h d f = if (d <= 0) then [] else (f 0 : h (d - eps) (\t -> f (t + eps))) 
+-- This is a monad only if the total duration is non-zero
+data HybSym a = HybSym [(SymTraj a, SymBound a)] 
 
--- This is a bit random
-instance (Show x, Show s) => Show (Hyb s x) where
-    show (HConv d f r) = 
-      "duration: " ++ (show d) 
-      ++ "; at 0: " ++ (show (f 0)) 
-      ++ "; at " ++ (show d) ++ ": " ++ (show  r)
-
-    show (HDiv d f) = 
-      "duration: " ++ (show d) 
-      ++ "; at 0: " ++ (show (f 0)) 
-
-instance Functor (Hyb s) where
+instance Functor (HybSym) where
     fmap f p = p >>= return . f
 
-instance Applicative (Hyb s) where
+instance Applicative (HybSym) where
     pure = return
-    f <*> p = f >>= \g-> (p >>= return . g)
+    f <*> p = f >>= \g-> p >>= return . g
 
-instance Monad (Hyb s) where
+instance Monad (HybSym) where
+  return x = HybSym [(Traj undefined, Dur 0)]
+   
+  f >>= g = undefined 
+     -- here, we've got a problem, because we do not know,
+     -- what the last point of the input trajectory is
+  
 
-  -- The real trajectory must have the type [0,0) -> s,
-  -- so, there are no points on which we can evaluate it
-  return = HConv 0 (const undefined)
+instance MonadHybrid (HybSym) where
+  trajectory d f = HybSym [(Traj f, Dur d)]
+  
+  start (HybSym ((Traj f, _) : _))       = f 0 
+  start (HybSym ((OdeInit x _ , _) : _)) = x
+  start _ = undefined
 
-  -- Divergent trajectories are not extendable
-  (HDiv d f) >>= g = (HDiv d f)
-
-  -- Extend a convergent trajectory to the right 
-  (HConv d f r) >>= g = 
-      case g r of 
-        HConv d' h p -> HConv (d + d') g' p
-        HDiv  d' h   -> HDiv  (d + d') g' 
-      where g' x = if (x < d) then f x else evalTraj (g r) (x - d)   
-
-wait :: Double -> x -> Hyb x x
-wait d x = HConv d (const x) x
-
--- example: concatenate two trajectories
-main = sample ((wait 1 1) >> (wait 1 2)) 0.1
